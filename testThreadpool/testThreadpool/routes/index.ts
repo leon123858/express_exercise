960dfe326551d4049a1f634a652add4d53b1a25d
leon123858 <a0970785699@gmail.com>
2021-09-13 16:54:29 +0800

    a

--- testThreadpool/testThreadpool/routes/index.ts
+++ testThreadpool/testThreadpool/routes/index.ts
@@ -4,15 +4,29 @@
 import express = require('express');
 const router = express.Router();
 
-router.get('/', (req: express.Request, res: express.Response) => {
-	res.render('index', { title: 'Express' });
-});
+router.get(
+	'/',
+	(req: express.Request, res: express.Response, next: express.NextFunction) => {
+		console.log('In');
+		next();
+	},
+	(req: express.Request, res: express.Response) => {
+		setTimeout(() => {
+			// Disable caching for content files
+			console.log(new Date());
+			res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+			res.header('Pragma', 'no-cache');
+			res.header('Expires', '0');
+			res.render('index', { title: 'Express' });
+		}, 5000);
+	}
+);
 
 router.post('/', (req: express.Request, res: express.Response) => {
 	setTimeout(() => {
 		console.log(new Date());
 		res.status(200).end('OK');
-	}, 1000);
+	}, 3000);
 });
 
 export default router;
